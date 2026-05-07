775fcbb2dc1edb999d341caae5e82d0530df9e06
hisjeans <his7jeans@gmail.com>
2026-05-07 18:23:46 +0900

    Java 객체지향 기초 - LearningLog 클래스에 생성자 추가 - main에서 객체를 다양한 생성자를 통해 생성 - this 키워드를 활용한 여러가지 문법 학습

--- src/oop/contructor/LearningLog.java
+++ src/oop/contructor/LearningLog.java
@@ -8,13 +8,59 @@ public class LearningLog {
     int minutes;
     boolean publicLog;
 
+    //클래스 이름과 동일한 함수 선언
+    //생성자는 클래스의 이름과 대/소문자까지 완전히 일치해야
+    //메서드와 달리 리턴 타입이 존재하지 않음
+    //일반적인 메서드는 리턴 타입이 반드시 존재해야
+
+    //기본 생성자
+    LearningLog(){ //전달받은 값이 없기 때문에 그냥 생성 가능
+        System.out.println("기본 생성자 호출");
+
+    }
+    //생성자 장점: 중복 선언 가능
+    //생성자는 메서드처럼 매개값을 전달받을 수 있음
+    //전달받은 값을 이용해 필드 초기화할 수 있음
+    //생성자는 중복 선언 가능(오버로딩: overloading)
+    //이름이 동일하기 때문에 전달하는 값의 개수, 순서, 타입을 다르게 작성해야 중복 선언 인정
+    LearningLog(String title, int minutes){
+        //javaLog, gitLog, loopLog 모두 true이면 LearningLog를 생성할 때 true로 생성하면 되지 않을까
+
+        /*
+        this.title=title;
+        this.minutes=minutes;
+        this.publicLog=true;
+        "Learning:og(String title, int minutes, boolean publicLog"와 패턴 같음
+        */
+        this(title, minutes, true);
+
+        //this() 문법으로 자기 자신의 다른 생성자를 호출하는 것 가능
+        //title(string), minutes(int), true(boolean) 받을 수 있는 다른 생성자("Learning:og(String title, int minutes, boolean publicLog") 부를 것
+    }
+
+    LearningLog(String title, int minutes, boolean publicLog){ //객체 생성할 때 title, minutes, publiclog 보내주어야
+        //title, minutes, publiclog 한번에 전달 가능
+        this.title=title;
+        //객체가 가진 필드=매개값으로 전달받은 것 세팅
+        //but title=title(LearnigLog(String title)로 한다면 구분하지 못함 LearnigLog의 title에 LearningLog의 title을 대입하게 됨
+        //해결1: 필드명!=변수명 작성
+        //해결2: this 키워드 사용, 지금 생성되고 있는 그 생성자의 타이틀 'this.title'
+        //this는 현재 객체 자기 자신을 뜻함
+        //생성자에서의 this는 곧 지금 생성자에 의해 생성되고 있는 그 객체를 의미
+        //필드와 생성자의 매개변수 이름이 동일할 경우 this를 통해 구분지어 줘야 함
+
+        this.minutes=minutes;
+        this.publicLog=publicLog;
+
+    }
     //메서드 선언
     void printSummary(){ //반환해주는 것이 없기 때문에 void
         System.out.println(title+" - "+minutes+"분");
-    }
+    }//this.title, this.miutes 생략된 것, 이 메서드를 호출한 그 객체의 title, minutes
 
     boolean needsReview(){
         return minutes<60;
-    }
+    } //메서드를 호출한 그 객체의 minutes
+    //메서드에서 특정 필드 지정하기 위해서도 this
 
 }
---
+++ src/oop/contructor/SpringLogApp.java
@@ -0,0 +1,31 @@
+package oop.contructor;
+
+public class SpringLogApp {
+    public static void main(String[] args) {
+
+
+        //객체 생성
+        LearningLog javalog=new LearningLog("Java 시작",40/*,true*/);
+        //'LearnigLog()' 생성자
+        //"LearnigLog(String title, int minutes, boolean publicLog가 받음
+        //객체 생성하자마자 printsummary 호출하면 생성자가 호출되고 title, minutes 값을 넣지 않아 null이 나옴
+        //컴파일러가 코드를 바이트 코드로 번역하고 컴퓨터에게 전달
+        //JVM(Java Virtual Machine)이 자바 실행될 수 있는 가상 환경(JRE) 만듦
+        //JVM만 존재한다면 맥, 윈도우 무관하게 동작 가능
+        //JVM이 main 메서드 호출해 main 메서드 안의 내용 실행되는 것
+
+        LearningLog gitLog=new LearningLog("Git 복습",30,false);
+        //생성자가 실수 방지해줄 수 있음 하나라도 빠지면 알려줌
+        //public log가 false가 될 때만 전달해줌
+        LearningLog conditionLog=new LearningLog("조건문 학습",75/*,true*/);
+
+        LearningLog loopLog=new LearningLog("반복문 학습",120/*,true*/);
+        //javaLog, gitLog, loopLog 모두 true이면 LearningLog를 생성할 때 true로 생성하면 되지 않을까
+
+        LearningLog[] logs={javalog, gitLog, conditionLog, loopLog};
+        System.out.println("=== 생성자로 만든 학습 기록 ===");
+        for(LearningLog log:logs){
+            log.printSummary();
+        }
+    }
+}
